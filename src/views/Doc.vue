da962938ae079e5f210524701202ba3bfb7da3db
1446450047 <1446450047@qq.com>
2020-08-06 17:17:49 +0800

    添加了aside的侧边栏html

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,19 +1,34 @@
 <template>
     <div>
-        <div class="topNav">
-            <div class="log"></div>
-            <div class="menu"></div>
-        </div>
+        <TopNav/>
         <div class="content">
-            <aside>侧边栏</aside>
+            <aside>
+                <h2>组件列表</h2>
+                <ol>
+                    <li>
+                        <router-link to="/document/">Switch组件</router-link>
+                    </li>
+                    <li>
+                        <router-link to="/document/">Button组件</router-link>
+                    </li>
+                    <li>
+                        <router-link to="/document/">Dialog组件</router-link>
+                    </li>
+                    <li>
+                        <router-link to="/document/">Tabs组件</router-link>
+                    </li>
+                </ol>
+            </aside>
             <main>主内容</main>
         </div>
     </div>
 </template>
 
 <script lang="ts">
+    import TopNav from "../components/TopNav.vue";
     export default {
-        name: "Doc"
+        name: "Doc",
+        components: {TopNav}
     };
 </script>
 
