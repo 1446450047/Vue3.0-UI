60e3a9e9b5952a39324b15690fb28f07e34dd75f
1446450047 <1446450047@qq.com>
2020-11-03 04:50:11 +0800

    完成下划线切换动画

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,12 +1,13 @@
 <template>
   <div class=" Vue3-Tabs">
-    <div class="Vue3-Tabs-nav">
+    <div class="Vue3-Tabs-nav" ref="container">
       <div class="Vue3-Tabs-item"
            :class="{selected : t=== selected}"
            @click="select(t)"
+           :ref="el=>{if(el) navItems[index] = el}"
            v-for="(t, index) in titles " :key="index">{{ t }}
       </div>
-      <div class="Vue3-Tabs-item-indicator"></div>
+      <div class="Vue3-Tabs-item-indicator" ref="indicator"></div>
     </div>
     <div class="Vue3-Tabs-content">
       <component class="Vue3-Tabs-content-item" v-for="(c) in defaults"
@@ -17,6 +18,7 @@
 
 <script lang="ts">
 import Tab from "./Tab.vue";
+import {ref, onMounted, onUpdated} from "vue";
 
 export default {
   name: "Tabs",
@@ -27,7 +29,20 @@ export default {
     }
   },
   setup(props: any, context: any) {
+    const navItems = ref<HTMLDivElement[]>([]);
     const defaults = context.slots.default();
+    const indicator = ref<HTMLDivElement>();
+    const container = ref<HTMLDivElement>()
+    const x = () => {
+      const divs = navItems.value;
+      const result = divs.filter(div => div.classList.contains("selected"))[0];
+      const {width,left:left2} = result.getBoundingClientRect();
+      indicator.value && (indicator.value.style.width = width + "px");
+      const left1 = container.value && container.value.getBoundingClientRect().left || 0;
+      indicator.value && (indicator.value.style.left = left2 - left1 + "px");
+    };
+    onMounted(x)
+    onUpdated(x)
     defaults.forEach((tag: any) => {
       if (tag.type !== Tab) {
         throw new Error("Tabs 的 子组件必须为Tab组件");
@@ -36,15 +51,15 @@ export default {
     const titles = defaults.map((tag: any) => {
       return tag.props.title;
     });
-    const select = (t:string) => {
-        context.emit("update:selected",t)
+    const select = (t: string) => {
+      context.emit("update:selected", t);
     };
-    return {defaults, titles, select};
+    return {defaults, titles, select, navItems, indicator,container};
   }
 };
 </script>
 
-<style  lang="scss">
+<style lang="scss">
 $border-color: #d9d9d9;
 $blue: #40a9ff;
 .Vue3-Tabs {
@@ -56,16 +71,19 @@ $blue: #40a9ff;
   }
 
   &-item {
-    padding: 8px 16px;
-
+    padding: 8px 0;
+    margin: 0 16px;
+    cursor: pointer;
     &:first-child {
-      padding-left: 0;
+      margin-left: 0;
     }
 
     &.selected {
       color: $blue;
     }
-    &-indicator{
+
+    &-indicator {
+      transition: all 250ms;
       position: absolute;
       bottom: 0;
       left: 0;
@@ -77,8 +95,10 @@ $blue: #40a9ff;
 
   &-content {
     padding-top: 8px;
-  &-item{
+
+    &-item {
       display: none;
+
       &.selected {
         display: block;
       }
